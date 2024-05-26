1ea46ba1906e41b93744b800066a58a432bd6c84
brandonviry <brandonviry@gmail.com>
2024-05-26 13:30:35 +0400

    Clear code
    I have cleaned up the source code

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,5 @@
 hljs.initHighlightingOnLoad();
-// $(document).ready(function () {
-//   $(".navbar-burger").click(function () {
-//     $(".navbar-burger").toggleClass("is-active");
-//     $(".navbar-menu").toggleClass("is-active");
-//   });
-// });
+
 $(".section-content").hide();
 $(document).ready(function () {
   $(".hero").click((e) => {
